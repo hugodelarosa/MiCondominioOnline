d81f52e5e311dab27f25241ea39e9e4a6f71755d
Alex Torres <alejandro.torres@condominio.plus>
2017-03-09 00:03:17 -0600

    created role migration

--- database/migrations/2017_03_09_054315_create_role_table.php
+++ database/migrations/2017_03_09_054315_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->bigInteger('Id');
             $table->text('Name');
-            $table->text('Last name');
-            $table->text('Email');
-            $table->string('Telephone');
-            $table->text('Password');
-            $table->dateTime(created_at);
-            $table->dateTime(updated_at);
-        });
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->dateTime('deleted_at');
+        })
     }
 
     /**
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('role');
     }
 }
