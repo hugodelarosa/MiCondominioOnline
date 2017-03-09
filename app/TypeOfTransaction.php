2ae3a1ed469d8297f56d7a30b96f8c951dfb34aa
Alex Torres <alejandro.torres@condominio.plus>
2017-03-09 12:34:04 -0600

    created models

---
+++ app/TypeOfTransaction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeOfTransaction extends Model
+{
+    //
+}
---
+++ app/UnlockedFeature.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UnlockedFeature extends Model
+{
+    //
+}
