733b3f34fea5f83acfe906cac5000812a4b0b07f
Alex Torres <alejandro.torres@condominio.plus>
2017-03-10 12:59:17 -0600

    fixed bug in phone form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,7 +57,7 @@
                             <label for="phone" class="col-md-4 control-label">Phone Number</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="phone" value="{{ old('phone') }}" required>
+                                <input id="phone" type="phone" class="form-control" name="phone" value="{{ old('phone') }}" required>
 
                                 @if ($errors->has('phone'))
                                     <span class="help-block">
