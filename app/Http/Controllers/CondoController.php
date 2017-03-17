0dcd757f0e2f4c7a32bedb01e869066caab3da3a
Alex Torres <alejandro.torres@condominio.plus>
2017-03-17 10:51:53 -0600

    created condo controller and index view

---
+++ app/Http/Controllers/CondoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Condo;
+use Illuminate\Http\Request;
+
+class CondoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Condo::all();
+        return view('condo.index', compact('data'))
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Condo  $condo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Condo $condo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Condo  $condo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Condo $condo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Condo  $condo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Condo $condo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Condo  $condo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Condo $condo)
+    {
+        //
+    }
+}
