83755c4034f3f95e7a809a8497168637bbf9c874
Alex Torres <alejandro.torres@condominio.plus>
2017-03-17 11:37:12 -0600

    created show and edit views

---
+++ resources/views/condo/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.dashboard')
+@extends('layouts.app')
+
+@section('content')
+
+@endsection
+
+@section('dashboard')
+<div class="col-md-8">
+    <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+            <h5 style="padding-top: 1.5px;" class="pull-left">Editar condominio {{ $condo->name }}</h5>
+            <a href="" class="btn btn-default pull-right" href="{{ route('condo.index') }}">Atras</a>
+        </div>
+        <div class="panel-body">
+            <form class="form-horizontal" role="form" method="POST" action="{{ route('condo.update', $condo->id) }}">
+                {{ method_field('PUT') }}
+                {{ csrf_field() }}
+                
+                <div class="form-group">
+                    <strong>Condominio:</strong>
+                    <input class="form-control" type="text" name="name">
+                </div>
+                <div class="form-group">
+                    <strong>Dirección:</strong>
+                    <input class="form-control" type="text" name="direction">
+                </div>
+                <button type="submit" class="btn btn-primary">Editar</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
