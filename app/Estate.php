9603f9580e4d99ce7ae8b3497f4ea0ba292a6e61
Alex Torres <alejandro.torres@condominio.plus>
2017-03-17 21:52:17 -0600

    made estate form fields fillable and register view

--- app/Estate.php
+++ app/Estate.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Estate extends Model
 {
+    protected $fillable = [
+        'number', 'rented', 'number_of_parking_lots', 'notes'
+    ];
+    
     public function typeOfEstate()
     {
         return $this->belongsTo('App\TypeOfEstate');
